e14308a25c14ba199e8b16a3e6277595cbbb02af
MitchGianoni <mitchgianoni@yahoo.com>
2018-06-15 12:15:58 -0400

    rendering shopping list complete

--- index.js
+++ index.js
@@ -19,7 +19,17 @@ const STORE = [
 
 function generateItemElement(item, itemIndex, template) {
   return `
-    <li>${item.name}</li>`;
+    <li class="js-item-index-element" data-item-index="${itemIndex}">
+      <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}">${item.name}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle js-item-toggle">
+            <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete js-item-delete">
+            <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`;
 }
 
 function generateShoppingItemsString(shoppingList) {
